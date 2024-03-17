eac1bc25afd2616d3a801893f2b21cf77d0a4d99
Vila71 <lolivila71@gmail.com>
2024-03-17 17:36:07 +0100

    create Api

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ArticleController;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::controller(CategoryController::class)->group(function() {
-    Route::get('/categories',  'index' );
-    Route::get('/categories/{id}',  'show' );
-    Route::post('/categories',  'store' );
-    Route::put('/categories/{id}', 'update');
-    Route::delete('/categories/{id}', 'destroy');
-});
-
-Route::controller(ArticleController::class)->group(function() {
-    Route::get('/articles',  'index' );
-    Route::get('/articles/{id}',  'show' );
-    Route::post('/articles',  'store' );
-    Route::put('/articles/{id}', 'update');
-    Route::delete('/articles/{id}', 'destroy');
-});
+Route::resource('categories', CategoryController::class);
+Route::resource('articles', ArticleController::class);
